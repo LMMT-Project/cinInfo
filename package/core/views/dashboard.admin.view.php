32f12a86b29d4b4cb0f8a8fe1c515b1efb784369
Théo Constant <59777055+tosu-dev@users.noreply.github.com>
2023-01-08 22:03:26 +0100

    Delete package directory

--- package/core/views/dashboard.admin.view.php
+++
@@ -1,9 +0,0 @@
-<?php use CMW\Manager\Lang\LangManager;
-use CMW\Utils\Utils;
-$title = LangManager::translate("core.dashboard.title");
-$description = LangManager::translate("core.dashboard.desc"); 
-?>
-
-<h3>
-    <span class="m-lg-auto">Tableau de bord</span>
-</h3>
